85bac68481e29eb87c682d99d8c6783b48addf1d
icanhazbroccoli <me@4pcbr.com>
2016-12-05 22:31:41 +0100

    Fixed the disonnected modem problem

--- data_provider/lib/data_provider/serial_port.ex
+++ data_provider/lib/data_provider/serial_port.ex
@@ -4,15 +4,15 @@ defmodule DataProvider.SerialPort do
   import Nerves.UART
   require Logger
 
-  @port_baud 9600
+  @port_baud 9_600
+  @timer_retry_hold 2_000
   @modem_type "usbmodem"
 
   def start_link() do
     GenServer.start_link(__MODULE__, %{pid: nil}, name: __MODULE__)
   end
 
-  def init(state) do
-    {:ok, pid}= Nerves.UART.start_link
+  defp open_port(pid) do
     port_name= Nerves.UART.enumerate
           |> Map.keys
           |> Enum.find(fn path ->
@@ -24,10 +24,20 @@ defmodule DataProvider.SerialPort do
     case port_name do
       nil ->
         Logger.error "Can not find the modem"
+        {:error, "Can not find the modem"}
       _ ->
         Logger.debug "Found modem: #{port_name}"
-        Nerves.UART.open(pid, port_name, speed: @port_baud, active: true)
+        Nerves.UART.open(pid, port_name, speed: @port_baud,
+                                        active: true,
+                            rx_framing_timeout: 5_000)
+        {:ok, port_name}
     end
+  end
+
+
+  def init(state) do
+    {:ok, pid}= Nerves.UART.start_link
+    open_port(pid)
     {:ok, Map.put(state, :pid, pid)}
   end
 
@@ -36,11 +46,20 @@ defmodule DataProvider.SerialPort do
   end
 
   def handle_call({:write, data}, _from, state= %{pid: pid}) do
-    case pid do
+    status= case pid do
       nil ->
         Logger.warn "The modem pid is empty, the data would be dropped on the floor"
-      _ -> Nerves.UART.write(pid, data)
+        nil
+      _ ->
+        case Nerves.UART.write(pid, data) do
+          :ok -> :ok
+          {:error, :ebadf} ->
+            # Probably the modem has been disconnected
+            :timer.sleep @timer_retry_hold
+            open_port(pid)
+        end
     end
+    Logger.debug inspect(status)
     {:reply, :ok, state}
   end
 
